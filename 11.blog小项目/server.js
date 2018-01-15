9d202741e9071567b817701eaa1bb22dad67fb66
dengzhao <1301829682@qq.com>
2018-01-15 23:31:33 +0800

    blog

--- 11.blog小项目/server.js
+++ 11.blog小项目/server.js
@@ -5,6 +5,10 @@ const cookieSession=require('cookie-session');
 const bodyParser=require('body-parser');
 const multer=require('multer');
 const consolidate=require('consolidate');
+const mysql=require('mysql');
+
+// 连接数据库
+const db = mysql.createPool({host: 'localhost', user: 'root', password: 'mysql', database: 'blog'})
 
 var server=express();
 server.listen(8080);
@@ -31,13 +35,38 @@ server.set('views', './template');
 //哪种模板引擎
 server.engine('html', consolidate.ejs);
 
-//接收用户请求
-server.get('/', function (req, res){
-  res.render('index.ejs', {});
+
+//接收用户请求，请求同一个地址的数据依次查询数据库获得，用next()实现链式操作
+server.get('/', (req, res, next) => {
+	//查询banner数据
+	res.banners = []
+	db.query('SELECT * FROM banner_table', (err,data) => {
+		if(err){
+			res.status(500).send('database error').end();
+		}else{
+			res.banners = data;
+		}
+		next();
+	});
 });
+server.get('/', (req, res, next)=>{
+  //查询文章列表
+  res.articles = []
+  db.query('SELECT ID,title,summary FROM article_table', (err, data)=>{
+    if(err){
+      res.status(500).send('database error').end();
+    }else{
+      res.articles=data;
+    }
+    next();
+  });
+});
+server.get('/', (req,res) => {
+	console.log(res.articles);
+	res.render('index.ejs', {banners: res.banners, articles: res.articles})
+})
+
 
 //5.请求静态资源目录
 server.use(static('./www'));
-
-
 //访问http://127.0.0.1:8080/就可以输出./template/index.ejs的结果
